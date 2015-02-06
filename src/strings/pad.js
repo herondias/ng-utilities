9715509b85336e0ea19f8959680bfd42a686057c
Julien Baillagou <julien.baillagou@decasoft.fr>
2015-02-06 19:49:01 +0100

    Implement Fractional Part. Implement leftPad and rightPad filters.

---
+++ src/strings/pad.js
@@ -0,0 +1,37 @@
+'use strict';
+
+angular.module('ngUtilities.strings', [])
+    /**
+     * Defines a filter to left-pad a value.
+     */
+    .filter('nguPadLeft', function () {
+        return function (input, pad, length) {
+            // if not number.
+            if(angular.isString(input)) {
+                var l = parseInt(length) - input.length;
+                for(var i = 0; i < l; i++) {
+                    input = pad.toString() + input;
+                }
+                return input;
+            } else {
+                return undefined;
+            }
+        };
+    })
+    /**
+     * Defines a filter to right-pad a value.
+     */
+    .filter('nguPadRight', function () {
+        return function (input, pad, length) {
+            // if not number.
+            if(angular.isString(input)) {
+                var l = parseInt(length) - input.length;
+                for(var i = 0; i < l; i++) {
+                    input += pad.toString();
+                }
+                return input;
+            } else {
+                return undefined;
+            }
+        };
+    });
