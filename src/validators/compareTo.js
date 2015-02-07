6a910cf866bd8493e8fe86484adc24c3e956f131
Julien Baillagou <julien.baillagou@decasoft.fr>
2015-02-08 00:08:03 +0100

    Add compare validators.

--- src/validators/compareTo.js
+++ src/validators/compareTo.js
@@ -4,16 +4,100 @@ angular.module('ngUtilities.validators')
     /**
      * Ensure that a model value is equal to another one.
      */
-    .directive('nguValCompareTo', function() {
+    .directive('nguValEqualTo', function() {
         return {
             restrict: 'A',
             require: 'ngModel',
             scope: {
-                otherModelValue: '=nguValCompareTo'
+                otherModelValue: '=nguValEqualTo'
             },
             link: function(scope, element, attr, ctrl) {
-                ctrl.$validators.nguValCompareTo = function(value) {
+                ctrl.$validators.nguValEqualTo = function(value) {
                     return value === scope.otherModelValue;
+                };
+                scope.$watch('otherModelValue', function() {
+                    ctrl.$validate();
+                });
+            }
+        };
+    })
+
+    /**
+     * Ensure that a model value is less or equal to another one.
+     */
+    .directive('nguValLeTo', function() {
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            scope: {
+                otherModelValue: '=nguValLeTo'
+            },
+            link: function(scope, element, attr, ctrl) {
+                ctrl.$validators.nguValLeTo = function(value) {
+                    return value <= scope.otherModelValue;
+                };
+                scope.$watch('otherModelValue', function() {
+                    ctrl.$validate();
+                });
+            }
+        };
+    })
+
+    /**
+     * Ensure that a model value is less or equal to another one.
+     */
+    .directive('nguValLtTo', function() {
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            scope: {
+                otherModelValue: '=nguValLtTo'
+            },
+            link: function(scope, element, attr, ctrl) {
+                ctrl.$validators.nguValLtTo = function(value) {
+                    return value < scope.otherModelValue;
+                };
+                scope.$watch('otherModelValue', function() {
+                    ctrl.$validate();
+                });
+            }
+        };
+    })
+
+    /**
+     * Ensure that a model value is greater or equal to another one.
+     */
+    .directive('nguValGeTo', function() {
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            scope: {
+                otherModelValue: '=nguValGeTo'
+            },
+            link: function(scope, element, attr, ctrl) {
+                ctrl.$validators.nguValGeTo = function(value) {
+                    return value >= scope.otherModelValue;
+                };
+                scope.$watch('otherModelValue', function() {
+                    ctrl.$validate();
+                });
+            }
+        };
+    })
+
+    /**
+     * Ensure that a model value is greater or equal to another one.
+     */
+    .directive('nguValGtTo', function() {
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            scope: {
+                otherModelValue: '=nguValGtTo'
+            },
+            link: function(scope, element, attr, ctrl) {
+                ctrl.$validators.nguValGtTo = function(value) {
+                    return value > scope.otherModelValue;
                 };
                 scope.$watch('otherModelValue', function() {
                     ctrl.$validate();
--- test/spec/validators/compareTo.js
+++ test/spec/validators/compareTo.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('Directive: nguValCompareTo', function() {
+describe('Directive: nguValEqualTo', function() {
 
     // load the service's module
     beforeEach(angular.mock.module('ngUtilities'));
@@ -17,7 +17,7 @@ describe('Directive: nguValCompareTo', function() {
         $scope.testValue = null;
         $scope.testCompareValue = null;
 
-        el = $compile('<form name="form"><input type="text" name="testValue" ng-model="testValue"><input type="text" name="testCompareValue" ng-model="testCompareValue" ngu-val-compare-to="testValue"></form>')($scope);
+        el = $compile('<form name="form"><input type="text" name="testValue" ng-model="testValue"><input type="text" name="testCompareValue" ng-model="testCompareValue" ngu-val-equal-to="testValue"></form>')($scope);
         $scope.$digest();
         form = $scope.form;
     }));
@@ -38,3 +38,192 @@ describe('Directive: nguValCompareTo', function() {
         expect(form.testCompareValue.$valid).toBe(false);
     });
 });
+
+describe('Directive: nguValGeTo', function() {
+
+    // load the service's module
+    beforeEach(angular.mock.module('ngUtilities'));
+
+    var $compile,
+        $scope,
+        el,
+        form;
+
+    // inject filter.
+    beforeEach(inject(function(_$compile_, _$rootScope_) {
+        $compile = _$compile_;
+        $scope = _$rootScope_;
+        $scope.testValue = null;
+        $scope.testCompareValue = null;
+
+        el = $compile('<form name="form"><input type="text" name="testValue" ng-model="testValue"><input type="text" name="testCompareValue" ng-model="testCompareValue" ngu-val-ge-to="testValue"></form>')($scope);
+        $scope.$digest();
+        form = $scope.form;
+    }));
+
+    it('shall validate with equal values.', function() {
+        form.testValue.$setViewValue('3');
+        expect($scope.testValue).toEqual('3');
+        form.testCompareValue.$setViewValue('3');
+        expect($scope.testCompareValue).toEqual('3');
+        expect(form.testCompareValue.$valid).toBe(true);
+    });
+
+    it('shall validate with greater values.', function() {
+        form.testValue.$setViewValue('3');
+        expect($scope.testValue).toEqual('3');
+        form.testCompareValue.$setViewValue('4');
+        expect($scope.testCompareValue).toEqual('4');
+        expect(form.testCompareValue.$valid).toBe(true);
+    });
+
+    it('shall not validate with different values.', function() {
+        form.testValue.$setViewValue('3');
+        expect($scope.testValue).toEqual('3');
+        form.testCompareValue.$setViewValue('2');
+        expect($scope.testCompareValue).toBeUndefined();
+        expect(form.testCompareValue.$valid).toBe(false);
+    });
+});
+
+describe('Directive: nguValGtTo', function() {
+
+    // load the service's module
+    beforeEach(angular.mock.module('ngUtilities'));
+
+    var $compile,
+        $scope,
+        el,
+        form;
+
+    // inject filter.
+    beforeEach(inject(function(_$compile_, _$rootScope_) {
+        $compile = _$compile_;
+        $scope = _$rootScope_;
+        $scope.testValue = null;
+        $scope.testCompareValue = null;
+
+        el = $compile('<form name="form"><input type="text" name="testValue" ng-model="testValue"><input type="text" name="testCompareValue" ng-model="testCompareValue" ngu-val-gt-to="testValue"></form>')($scope);
+        $scope.$digest();
+        form = $scope.form;
+    }));
+
+    it('shall validate with greater values.', function() {
+        form.testValue.$setViewValue('3');
+        expect($scope.testValue).toEqual('3');
+        form.testCompareValue.$setViewValue('4');
+        expect($scope.testCompareValue).toEqual('4');
+        expect(form.testCompareValue.$valid).toBe(true);
+    });
+
+    it('shall not validate with equal values.', function() {
+        form.testValue.$setViewValue('3');
+        expect($scope.testValue).toEqual('3');
+        form.testCompareValue.$setViewValue('3');
+        expect($scope.testCompareValue).toBeUndefined();
+        expect(form.testCompareValue.$valid).toBe(false);
+    });
+
+    it('shall not validate with different values.', function() {
+        form.testValue.$setViewValue('3');
+        expect($scope.testValue).toEqual('3');
+        form.testCompareValue.$setViewValue('2');
+        expect($scope.testCompareValue).toBeUndefined();
+        expect(form.testCompareValue.$valid).toBe(false);
+    });
+});
+
+describe('Directive: nguValLeTo', function() {
+
+    // load the service's module
+    beforeEach(angular.mock.module('ngUtilities'));
+
+    var $compile,
+        $scope,
+        el,
+        form;
+
+    // inject filter.
+    beforeEach(inject(function(_$compile_, _$rootScope_) {
+        $compile = _$compile_;
+        $scope = _$rootScope_;
+        $scope.testValue = null;
+        $scope.testCompareValue = null;
+
+        el = $compile('<form name="form"><input type="text" name="testValue" ng-model="testValue"><input type="text" name="testCompareValue" ng-model="testCompareValue" ngu-val-le-to="testValue"></form>')($scope);
+        $scope.$digest();
+        form = $scope.form;
+    }));
+
+    it('shall validate with equal values.', function() {
+        form.testValue.$setViewValue('3');
+        expect($scope.testValue).toEqual('3');
+        form.testCompareValue.$setViewValue('3');
+        expect($scope.testCompareValue).toEqual('3');
+        expect(form.testCompareValue.$valid).toBe(true);
+    });
+
+    it('shall validate with lower values.', function() {
+        form.testValue.$setViewValue('3');
+        expect($scope.testValue).toEqual('3');
+        form.testCompareValue.$setViewValue('2');
+        expect($scope.testCompareValue).toEqual('2');
+        expect(form.testCompareValue.$valid).toBe(true);
+    });
+
+    it('shall not validate with greater values.', function() {
+        form.testValue.$setViewValue('3');
+        expect($scope.testValue).toEqual('3');
+        form.testCompareValue.$setViewValue('4');
+        expect($scope.testCompareValue).toBeUndefined();
+        expect(form.testCompareValue.$valid).toBe(false);
+    });
+});
+
+describe('Directive: nguValLtTo', function() {
+
+    // load the service's module
+    beforeEach(angular.mock.module('ngUtilities'));
+
+    var $compile,
+        $scope,
+        el,
+        form;
+
+    // inject filter.
+    beforeEach(inject(function(_$compile_, _$rootScope_) {
+        $compile = _$compile_;
+        $scope = _$rootScope_;
+        $scope.testValue = null;
+        $scope.testCompareValue = null;
+
+        el = $compile('<form name="form"><input type="text" name="testValue" ng-model="testValue"><input type="text" name="testCompareValue" ng-model="testCompareValue" ngu-val-lt-to="testValue"></form>')($scope);
+        $scope.$digest();
+        form = $scope.form;
+    }));
+
+    it('shall validate with lower values.', function() {
+        form.testValue.$setViewValue('3');
+        expect($scope.testValue).toEqual('3');
+        form.testCompareValue.$setViewValue('2');
+        expect($scope.testCompareValue).toEqual('2');
+        expect(form.testCompareValue.$valid).toBe(true);
+    });
+
+    it('shall not validate with equal values.', function() {
+        form.testValue.$setViewValue('3');
+        expect($scope.testValue).toEqual('3');
+        form.testCompareValue.$setViewValue('3');
+        expect($scope.testCompareValue).toBeUndefined();
+        expect(form.testCompareValue.$valid).toBe(false);
+    });
+
+    it('shall not validate with greater values.', function() {
+        form.testValue.$setViewValue('3');
+        expect($scope.testValue).toEqual('3');
+        form.testCompareValue.$setViewValue('4');
+        expect($scope.testCompareValue).toBeUndefined();
+        expect(form.testCompareValue.$valid).toBe(false);
+    });
+});
+
