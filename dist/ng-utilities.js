d126fb8d6d338b33b5a737bb4223c5e0364e80e3
Julien Baillagou <julien.baillagou@decasoft.fr>
2015-07-28 22:55:23 +0200

    Build project

--- dist/ng-utilities.js
+++ dist/ng-utilities.js
@@ -271,6 +271,40 @@ angular.module('ngUtilities.numbers')
     }]);
 'use strict';
 
+angular.module('ngUtilities.numbers')
+    /**
+     * Defines a filter to generate a range of values.
+     * Write as : [5] | nguRange or [1,5] | nguRange
+     */
+    .filter('nguRange', function() {
+        return function(input) {
+            if (!input || !angular.isArray(input)) {
+                return undefined;
+            }
+
+            var lowBound, highBound;
+            switch (input.length) {
+                case 1:
+                    lowBound = 0;
+                    highBound = parseInt(input[0]) - 1;
+                    break;
+                case 2:
+                    lowBound = parseInt(input[0]);
+                    highBound = parseInt(input[1]);
+                    break;
+                default:
+                    return undefined;
+            }
+            var result = [];
+            for (var i = lowBound; i <= highBound; i++) {
+                result.push(i);
+            }
+            return result;
+        };
+    });
+
+'use strict';
+
 angular.module('ngUtilities.strings')
     /**
      * Defines a filter to capitalize a String.
